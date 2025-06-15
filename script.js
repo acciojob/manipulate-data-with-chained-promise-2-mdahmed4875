3ce59dfebc9ee37ab228efbd348b05f7823a1260
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-06-15 09:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+function handler() {
+  let arr = [1, 2, 3, 4];
+  let output = document.getElementById("output");
+
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  })
+    .then((arr1) => {
+      return new Promise((resolve) => {
+        const even = arr1.filter((num) => num % 2 === 0);
+        setTimeout(() => {
+          output.textContent = even.join(",");
+          resolve(even);
+        }, 1000);
+      });
+    })
+    .then((arr2) => {
+      return new Promise((resolve) => {
+        const mul = arr2.map((num) => num * 2);
+        setTimeout(() => {
+          output.textContent = mul.join(",");
+          resolve(mul);
+        }, 2000);
+      });
+    });
+}
+
+window.onload = handler;
